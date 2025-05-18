1992876a80cd3445fe5947e5d8e67b4eafe24990
boomfly <boomfly@rambler.ru>
2025-05-18 11:14:13 +0500

    fix loadHydratables

--- svelte-loadable-client.js
+++ svelte-loadable-client.js
@@ -4,19 +4,23 @@ import { LOADED, load, findByResolved } from 'svelte-loadable'
 export { default as LoadableProvider } from './svelte-loadable-provider-client.svelte'
 export { default as Loadable, preloadAll, register } from 'svelte-loadable'
 
-export const loadHydratables = (id = '__hydratables__') => {
+export const loadHydratables = async (id = '__hydratables__') => {
   const hydratablesNode = document.getElementById(id)
-  if (hydratablesNode) {
-    const hydratables = EJSON.parse(hydratablesNode.innerText)
-    hydratablesNode.parentNode.removeChild(hydratablesNode)
-    return (hydratables.length > 0)
-      ? Promise.all(hydratables
-        .map(resolved => findByResolved(resolved))
-        .filter(loader => !LOADED.has(loader))
-        .map(loader => load(loader))
-      )
-      : new Promise((resolve) => { resolve() })
-  } else {
-    return new Promise((resolve) => { resolve() })
+  if (!hydratablesNode) {
+    return
   }
+
+  const hydratables = EJSON.parse(hydratablesNode.innerText)
+  hydratablesNode.parentNode.removeChild(hydratablesNode)
+  if (hydratables.length == 0) {
+    return
+  }
+  for (let resolved of hydratables) {
+    const loader = findByResolved(resolved)
+    if (LOADED.has(loader)) {
+      continue
+    }
+    await load(loader);
+  }
+  return
 }
